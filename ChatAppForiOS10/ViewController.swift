31925460ecc1e150c3be46edfd193e5e0f00d9c4
Rickey Hrabowskie <rickeyhrabowskie@rickeys-macbook-pro.local>
2016-11-29 04:41:49 -0600

    Initial Commit

---
+++ ChatAppForiOS10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatAppForiOS10
+//
+//  Created by Rickey Hrabowskie on 11/29/16.
+//  Copyright © 2016 Rickey Hrabowskie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
